447791891d6d6751b3ea1f920cad8c82a69a51d9
Anurag Borah <anuragborah2548@gmai.com>
2025-06-19 18:36:25 +0100

    possible gateware

---
+++ artiq_kasli/possible/software/runtime/cargo/riscv32ima-unknown-none-elf/debug/deps/board_artiq-33733afb4e9e0456.d
@@ -0,0 +1,13 @@
+/home/artiq-alkaline/ARTIQ-alkaline-fork/artiq_kasli/possible/software/runtime/./cargo/riscv32ima-unknown-none-elf/debug/deps/board_artiq-33733afb4e9e0456.rmeta: libboard_artiq/lib.rs libboard_artiq/spi.rs libboard_artiq/mailbox.rs libboard_artiq/rpc_queue.rs libboard_artiq/si5324.rs libboard_artiq/grabber.rs libboard_artiq/drtio_routing.rs
+
+/home/artiq-alkaline/ARTIQ-alkaline-fork/artiq_kasli/possible/software/runtime/./cargo/riscv32ima-unknown-none-elf/debug/deps/libboard_artiq-33733afb4e9e0456.rlib: libboard_artiq/lib.rs libboard_artiq/spi.rs libboard_artiq/mailbox.rs libboard_artiq/rpc_queue.rs libboard_artiq/si5324.rs libboard_artiq/grabber.rs libboard_artiq/drtio_routing.rs
+
+/home/artiq-alkaline/ARTIQ-alkaline-fork/artiq_kasli/possible/software/runtime/./cargo/riscv32ima-unknown-none-elf/debug/deps/board_artiq-33733afb4e9e0456.d: libboard_artiq/lib.rs libboard_artiq/spi.rs libboard_artiq/mailbox.rs libboard_artiq/rpc_queue.rs libboard_artiq/si5324.rs libboard_artiq/grabber.rs libboard_artiq/drtio_routing.rs
+
+libboard_artiq/lib.rs:
+libboard_artiq/spi.rs:
+libboard_artiq/mailbox.rs:
+libboard_artiq/rpc_queue.rs:
+libboard_artiq/si5324.rs:
+libboard_artiq/grabber.rs:
+libboard_artiq/drtio_routing.rs:
